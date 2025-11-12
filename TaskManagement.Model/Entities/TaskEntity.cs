a6ae693e18ad8a2f75f8e6ba4956326f2b9926e4
MohammadJavad <mohammadjs2642@gmail.com>
2025-11-12 06:57:42 +0330

    task Management project added

---
+++ TaskManagement.Model/Entities/TaskEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace TaskManagement.Model.Entities;
+
+public class TaskEntity : BaseEntity
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public short State { get; set; }
+}
